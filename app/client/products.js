12702c10cf06b89fa811095210269bd8b27d5580
emelydvera <emely.vera@mercadolibre.com.co>
2022-08-05 08:18:27 -0500

    add i18n

--- app/client/products.js
+++ app/client/products.js
@@ -1,18 +1,22 @@
 const React = require('react');
 const hydrate = require('nordic/hydrate');
-const ProductsView = require('../pages/products/view');
+const I18n = require('nordic/i18n');
+const I18nProvider = require('nordic/i18n/I18nProvider');
 const ImageProvider = require('nordic/image/provider');
+const ProductsView = require('../pages/products/view');
 
-const { imagesPrefix } = window.__PRELOADED_STATE__;
+const {
+  imagesPrefix,
+  translations
+} = window.__PRELOADED_STATE__;
 
+const i18n = new I18n({ translations })
 
 hydrate(
-
-
+  <I18nProvider i18n={i18n}>
     <ImageProvider prefix={imagesPrefix}>
-        <ProductsView />
+      <ProductsView />
     </ImageProvider>
-
-    , document.getElementById('root-app')
-
-)
+  </I18nProvider>,
+  document.getElementById('root-app'),
+);
